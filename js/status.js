cbdbcd133e024b48762ea7ec72fbcbf252540c1a
dblock <dblock@dblock.org>
2015-12-28 16:57:50 -0500

    Add a banner that says how many active teams played how many games.

---
+++ js/status.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $.ajax({
+    type: "GET",
+    url: "http://bots.playplay.io/status",
+    success: function(data) {
+      var active_teams_count = 0;
+      var users_count = 0;
+      var matches_count = 0;
+      for (var game in data.games) {
+        game = data.games[game];
+        active_teams_count += game.active_teams_count;
+        users_count += game.users_count;
+        matches_count += game.matches_count;
+      }
+      $('#active_teams_count').hide().text(
+        active_teams_count + " active teams with " + users_count + " users and " + matches_count + ' games played.'
+      ).fadeIn('slow');
+    },
+  });
+
+});
