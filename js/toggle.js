6b580592707e12e9c17e468831245120dbb0d919
dblock <dblock@dblock.org>
2016-06-19 11:51:21 -0400

    Site has moved.

--- js/toggle.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#games-toggle').on('click', function(e) {
-    e.preventDefault();
-    $('#games-pong').fadeOut('fast', function() {
-      $('#games-all').fadeIn('slow');
-    });
-  });
-});
-
